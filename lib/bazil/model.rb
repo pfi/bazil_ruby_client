481a60ef2e654dd54655e4c92336c122ab485ded
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2012-01-10 15:54:46 +0900

    Implemented train and classify in Model

--- lib/bazil/model.rb
+++ lib/bazil/model.rb
@@ -19,7 +19,29 @@ module Bazil
       JSON.parse(res.body)
     end
 
+    # TODO: config
+    # TODO: retrain
+    # TODO: label APIs
+
+    def train(label, data)
+      data = %({"label": "#{label}", "data": #{data.to_json}})
+      post('train', data, "Failed to post training data")
+      true
+    end
+
+    def classify(data)
+      data = %({"data": #{data.to_json}})
+      res = JSON.parse(post('classify', data, "Failed to post data for classification"))
+      return res['max_label'], res
+    end
+
     private
+    def post(path, data, error_message)
+      res = @http_cli.post(gen_uri(path), data, {'Content-Type' => 'application/json; charset=UTF-8', 'Content-Length' => data.length.to_s})
+      raise error_message + ": application = #{@application.name}, model = #{@name}" unless res.code =~ /2[0-9][0-9]/ # TODO: enhance error information
+      res.body
+    end
+
     def gen_uri(path = nil)
       if path
         "/apps/#{@application.name}/models/#{@name}/#{path}"
