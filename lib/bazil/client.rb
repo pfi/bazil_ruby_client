97b1bdd8e03ae6072bb27419bf1bde770fb71fc1
Masahiro Nakagawa <nakagawa@preferred.jp>
2012-06-14 22:07:25 +0900

    Apply #6416 changes. refs #6416

--- lib/bazil/client.rb
+++ lib/bazil/client.rb
@@ -55,7 +55,8 @@ module Bazil
     end
 
     def create_application(name, config = {})
-      data = {:application_name => name, :config => config}.to_json
+      config['application_name'] = name
+      data = config.to_json
       res, body = @http_cli.post(gen_uri('apps'), data, {'Content-Type' => 'application/json; charset=UTF-8', 'Content-Length' => data.length.to_s})
       raise_error("Failed to create application: #{name}", res) unless res.code =~ /2[0-9][0-9]/ # TODO: return detailed error information
       Application.new(self, name)
