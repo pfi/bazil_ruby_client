86b79cc904fe77a959a3cfee68db408ab321c9b8
TAKEI Yuya <takei@preferred.jp>
2013-02-06 15:32:38 +0900

    Remove old Implements
    refs #11

--- lib/bazil/client.rb
+++ lib/bazil/client.rb
@@ -84,37 +84,6 @@ module Bazil
 
     def_delegators :@http_cli, :read_timeout, :read_timeout=, :set_api_keys
 
-    def status
-      res = @http_cli.get(gen_uri('status'))
-      raise_error("Failed to get status of the server", res) unless res.code =~ /2[0-9][0-9]/
-      JSON.parse(res.body)
-    end
-
-    def config
-      res = @http_cli.get(gen_uri('config'))
-      raise_error("Failed to get config of the server", res) unless res.code =~ /2[0-9][0-9]/
-      JSON.parse(res.body)
-    end
-
-    def update_config(config)
-      data = config.to_json
-      res = @http_cli.put(gen_uri('config'), data, {'Content-Type' => 'application/json; charset=UTF-8', 'Content-Length' => data.length.to_s})
-      raise_error("Failed to update config of the server", res) unless res.code =~ /2[0-9][0-9]/
-      JSON.parse(res.body)
-    end
-
-    def errors
-      res = @http_cli.get(gen_uri('errors'))
-      raise_error("Failed to get information of errors from the server", res) unless res.code =~ /2[0-9][0-9]/
-      JSON.parse(res.body)
-    end
-
-    def clear_errors
-      res = @http_cli.delete(gen_uri('errors'))
-      raise_error("Failed to clear error information of the server", res) unless res.code =~ /2[0-9][0-9]/
-      true
-    end
-
     def models
       res, body = @http_cli.get(gen_uri('models'))
       raise_error("Failed to get models", res) unless res.code =~ /2[0-9][0-9]/
@@ -133,6 +102,13 @@ module Bazil
       res, body = @http_cli.delete(gen_uri("models/#{model_id}"))
       raise_error("Failed to delete model", res) unless res.code =~ /2[0-9][0-9]/ # TODO: return detailed error information
       model_id
+    end
+    def create_config(model_id, config)
+      TODO: impl
+    end
+
+    def delete_config(model_id, config_id)
+      TODO: impl
     end
 
     def model(model_id, config_id)
